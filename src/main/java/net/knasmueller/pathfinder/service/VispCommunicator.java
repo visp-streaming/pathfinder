bba9f20c10f747c22b8c2d44d913c78eea57c532
Bernhard Knasmüller <b.knasmueller@gmail.com>
2017-04-11 21:05:05 +0200

    current topology is automatically fetched from VISP

--- src/main/java/net/knasmueller/pathfinder/service/VispCommunicator.java
+++ src/main/java/net/knasmueller/pathfinder/service/VispCommunicator.java
@@ -4,7 +4,10 @@ import net.knasmueller.pathfinder.entities.VispRuntimeIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +25,7 @@ public class VispCommunicator {
             LOG.error("Invalid endpoint");
         } else {
             vispRuntimeIdentifiers.add(endpoint);
+            getTopology(endpoint);
             LOG.debug("Added endpoint " + endpoint);
         }
     }
@@ -38,5 +42,19 @@ public class VispCommunicator {
 
     public List<VispRuntimeIdentifier> getVispRuntimeIdentifiers() {
         return vispRuntimeIdentifiers;
+    }
+
+    public String getTopology(VispRuntimeIdentifier rt) {
+        RestTemplate restTemplate = new RestTemplate();
+        URI targetUrl = UriComponentsBuilder.fromUriString("http://" + rt)
+                .path("/getTopology")
+                .build()
+                .toUri();
+        LOG.debug("targetUrl for getTopology call: " + targetUrl);
+        String topology = restTemplate.getForObject(targetUrl, String.class);
+        LOG.debug("Topology equals to: ");
+        LOG.debug(topology);
+
+        return topology;
     }
 }
