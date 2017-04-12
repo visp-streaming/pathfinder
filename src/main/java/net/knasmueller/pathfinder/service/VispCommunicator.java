46724b57f06a36a7579445c3e7aa7f582650abc9
Bernhard <b.knasmueller@gmail.com>
2017-04-12 17:50:09 +0200

    parse received topology using VISP topologyParser

--- src/main/java/net/knasmueller/pathfinder/service/VispCommunicator.java
+++ src/main/java/net/knasmueller/pathfinder/service/VispCommunicator.java
@@ -1,12 +1,18 @@
 package net.knasmueller.pathfinder.service;
 
+import ac.at.tuwien.infosys.visp.topologyParser.TopologyParser;
 import net.knasmueller.pathfinder.entities.VispRuntimeIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +25,11 @@ public class VispCommunicator {
     private static final Logger LOG = LoggerFactory.getLogger(VispCommunicator.class);
 
     List<VispRuntimeIdentifier> vispRuntimeIdentifiers = new ArrayList<>();
+
+    @Autowired
+    VispTopology vispTopology;
+
+    TopologyParser topologyParser = new TopologyParser();
 
     public synchronized void addVispRuntime(VispRuntimeIdentifier endpoint) {
         if (endpoint == null || "".equals(endpoint)) {
@@ -54,6 +65,7 @@ public class VispCommunicator {
         String topology = restTemplate.getForObject(targetUrl, String.class);
         LOG.debug("Topology equals to: ");
         LOG.debug(topology);
+        vispTopology.setTopology(topologyParser.parseTopologyFromString(topology).topology);
 
         return topology;
     }
---
+++ src/main/java/net/knasmueller/pathfinder/service/VispTopology.java
@@ -0,0 +1,19 @@
+package net.knasmueller.pathfinder.service;
+
+import ac.at.tuwien.infosys.visp.common.operators.Operator;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class VispTopology {
+    Map<String, Operator> topology;
+
+    public Map<String, Operator> getTopology() {
+        return topology;
+    }
+
+    public void setTopology(Map<String, Operator> topology) {
+        this.topology = topology;
+    }
+}
