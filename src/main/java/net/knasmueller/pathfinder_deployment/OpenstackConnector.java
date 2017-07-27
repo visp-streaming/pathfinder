86ab36d0cea2974ae2cfd49e70bf8d748fbf71a6
Bernhard Knasmueller <e1109965@student.tuwien.ac.at>
2017-07-27 13:51:44 +0200

    refactoring

--- src/main/java/net/knasmueller/pathfinder_deployment/OpenstackConnector.java
+++ src/main/java/net/knasmueller/pathfinder_deployment/OpenstackConnector.java
@@ -9,8 +9,6 @@ import org.openstack4j.model.compute.ServerCreate;
 import org.openstack4j.openstack.OSFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -36,14 +34,7 @@ public class OpenstackConnector {
                 .credentials(OS_USERNAME, OS_PASSWORD)
                 .tenantName(OS_TENANT_NAME)
                 .authenticate();
-        LOG.info("Connection successful");
     }
-
-//    public List<String> getFlavours() {
-//        setup();
-//        return os.compute().flavors().list().stream().map(i -> i.getName()).collect(Collectors.toList());
-//    }
-
 
     public String startInstance(String serverName) {
         setup();
@@ -63,71 +54,18 @@ public class OpenstackConnector {
         ServerCreate sc = Builders.server()
                 .name(serverName)
                 .flavor(flavor)
-                .image("3a233e8a-e3c9-4076-9cbf-7526dd32634b")
+                .image("3a233e8a-e3c9-4076-9cbf-7526dd32634b") // coreOS
                 .keypairName(KEYPAIR_NAME)
                 .addSecurityGroup("default")
                 .build();
 
-        Server server = os.compute().servers().boot(sc);
+        os.compute().servers().boot(sc);
 
-
-//        if (PUBLICIPUSAGE) {
-//            FloatingIP freeIP = null;
-//
-//            for (FloatingIP ip : os.compute().floatingIps().list()) {
-//                if (ip.getFixedIpAddress() == null) {
-//                    freeIP = ip;
-//                    break;
-//                }
-//            }
-//            if (freeIP == null) {
-//                freeIP = os.compute().floatingIps().allocateIP("cloud");
-//            }
-//
-//            try {
-//                Thread.sleep(3000);
-//            } catch (InterruptedException e) {
-//                //TODO remove if openstack behaves again
-//            }
-//
-//            ActionResponse ipresponse = os.compute().floatingIps().addFloatingIP(server, freeIP.getFloatingIpAddress());
-//            if (!ipresponse.isSuccess()) {
-//                LOG.error("IP could not be retrieved:" + ipresponse.getFault());
-//            }
-//            uri = freeIP.getFloatingIpAddress();
-//        }
-
-
-        LOG.info("VISP - Server with id: " + serverName + " was started.");
-
-        //wait until the dockerhost is available
-//        Boolean connection = false;
-//        while (!connection) {
-//            try {
-//                TimeUnit.SECONDS.sleep(1);
-//                final DockerClient docker = DefaultDockerClient.builder().
-//                        uri(URI.create("http://" + dh.getUrl() + ":2375")).
-//                        connectTimeoutMillis(100000).build();
-//                docker.ping();
-//                connection = true;
-//            } catch (InterruptedException | DockerException e) {
-//                LOG.debug("Dockerhost is not available yet.", e);
-//            }
-//        }
-//
-//        dhr.save(dh);
-//        sar.save(new ScalingActivity("host", new DateTime(DateTimeZone.UTC), "", "startInstance", dh.getName()));
+        LOG.info("Server with id: " + serverName + " was started.");
 
         return serverName;
     }
 
-    //    @Override
-//    public final void stopDockerHost(final DockerHost dh) {
-//        stopDockerHost(dh.getName());
-//        dhr.delete(dh);
-//        sar.save(new ScalingActivity("host", new DateTime(DateTimeZone.UTC), "", "stopWM", dh.getName()));
-//    }
-//
     public final void destroyInstanceByName(final String id) {
         setup();
 
