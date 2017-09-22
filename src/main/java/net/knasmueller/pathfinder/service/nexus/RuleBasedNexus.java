8f2ab5f4e3d81819bba3e2b616f49c3b52a5e235
Bernhard Knasmueller <e1109965@student.tuwien.ac.at>
2017-09-22 15:32:40 +0200

    fine-tuned RuleBasedNexus

--- src/main/java/net/knasmueller/pathfinder/service/nexus/RuleBasedNexus.java
+++ src/main/java/net/knasmueller/pathfinder/service/nexus/RuleBasedNexus.java
@@ -14,14 +14,22 @@ public class RuleBasedNexus implements INexus {
     @Override
     public OperatorClassification predict(SingleOperatorStatistics s) {
         if(s.getItemsWaiting() > 20 && s.getActualCpuCores() < 0.1) {
+            // process does no work despite full queue
             return OperatorClassification.FAILED;
         }
 
-        if(s.getActualMemory() < 50 && s.getActualCpuCores() < 0.1) {
+        if(s.getActualMemory() < 150 && s.getActualCpuCores() < 0.1) {
+            // too small resource usage for productive work
             return OperatorClassification.FAILED;
         }
 
         if(s.getItemsWaiting() > 100 && s.getDeliveryRate() < 0.5) {
+            // no output despite full queue
+            return OperatorClassification.FAILED;
+        }
+
+        if(s.getDeliveryRate() < 0.5 && s.getActualCpuCores() > 1.6) {
+            // process most likely stuck in a loop
             return OperatorClassification.FAILED;
         }
 
